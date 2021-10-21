3b6d95aa6a926fe5136ec4b79f5586a7a06b0534
Bladerendeeg <41634346+Bladerendeeg@users.noreply.github.com>
2021-10-21 15:59:34 +0200

    nice

---
+++ script.js
@@ -0,0 +1,15 @@
+// // zet de functie om naar een arrow functie
+// const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+
+// ikRockArrowFunctions()
+
+let a = 12;
+
+const fivePlusSeven = myFunction => a + 5
+
+fivePlusSeven()
+
+const createObject = () => ({greeting: "hoi"})
+
+createObject()
+
